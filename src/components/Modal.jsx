a14f0afe0c202e81057bbc7f13967cf799abdec0
Joell Jones <25757151+joelljones@users.noreply.github.com>
2023-09-06 14:52:37 -0400

    feat: create work page

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -115,31 +115,9 @@ export default function MyModal() {
 															</a>
 															<a
 																class="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
-																href="/about"
-															>
-																About Us
-																<span class="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100"></span>
-															</a>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="even:mt-px sm:bg-neutral-950">
-												<div class="mx-auto max-w-7xl px-6 lg:px-8">
-													<div class="mx-auto max-w-2xl lg:max-w-none">
-														<div class="grid grid-cols-1 sm:grid-cols-2">
-															<a
-																class="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
 																href="/process"
 															>
 																Our Process
-																<span class="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100"></span>
-															</a>
-															<a
-																class="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
-																href="/blog"
-															>
-																Blog
 																<span class="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100"></span>
 															</a>
 														</div>
